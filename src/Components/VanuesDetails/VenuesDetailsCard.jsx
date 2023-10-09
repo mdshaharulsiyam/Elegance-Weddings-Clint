2152d24b9a69b821944011d356d8cea13f7f71d6
shaharul siyam <shaharulsiyam0273@.com>
2023-10-09 12:06:17 +0600

    full project responsive done

--- src/Components/VanuesDetails/VenuesDetailsCard.jsx
+++ src/Components/VanuesDetails/VenuesDetailsCard.jsx
@@ -38,8 +38,8 @@ const VenuesDetailsCard = ({ data }) => {
         <div className='my-10'>
             <img className='w-full object-cover' src={image} alt="" />
             <div className='flex justify-start items-center gap-3 text-pink-600 font-bold italic text-xl my-5'><h3>{placeTitle}</h3> <p className='px-2 py-1 bg-pink-600 text-black rounded-lg bg-opacity-50'>{place}</p></div>
-            <div className='flex justify-center gap-[2%] items-center'>
-                <div className='w-[49%]'>
+            <div className='flex justify-center flex-col sm:flex-row gap-[2%] items-center'>
+                <div className='sm:w-[49%] w-full'>
                     <span className='font-extrabold text-xl text-red-600'>Venue Policies</span>
                     <ul className='list-disc pl-10'>
                         <li className='text-lg font-extrabold'>advance : <span className='text-pink-600'>{advance}</span> </li>
@@ -49,14 +49,14 @@ const VenuesDetailsCard = ({ data }) => {
                         <li className='text-lg font-extrabold'>averagePriceFood : <span className='text-pink-600'>{averagePriceFood}</span> </li>
                     </ul>
                 </div>
-                <div className='w-[49%]'>
+                <div className='sm:w-[49%] w-full'>
                     <h4 className='text-xl font-bold uppercase'>description</h4>
                     <p className='py-4 tracking-[1px] font-semibold'>{description}</p>
                     <p className=' text-lg font-bold'>reviews : {reviews}</p>
                     <p className='flex justify-start items-center gap-2 text-lg font-bold'>ratings : {ratings} <AiFillStar></AiFillStar></p>
                 </div>
             </div>
-            <div className='flex justify-center items-center gap-4'>
+            <div className='flex justify-center flex-col sm:flex-row items-center gap-4'>
                 <button onClick={addtocart} className='btn bg-pink-600 bg-opacity-50 text-xl font-bold uppercase my-4 block '>add to cart</button>
                 <button onClick={handelbtn} className='btn bg-pink-600 bg-opacity-50 text-xl font-bold uppercase my-4 block '>back to previous</button>
             </div>
